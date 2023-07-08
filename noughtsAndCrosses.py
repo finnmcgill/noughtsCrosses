2cb7bc80eb530da59afb837768d30623ef95b3fb
Finn McGill <finn@finnmcgill.com>
2023-07-08 21:11:38 +0100

    Started on simple UI

---
+++ noughtsAndCrosses.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul  8 20:59:50 2023
+
+@author: finnj
+"""
+
+
+
+i = 0
+
+def openGame():
+    print("        1     2     3")
+    print("     -------------------")
+    print("A    I     I     I     I")
+    print("     -------------------")
+    print("B    I     I     I     I")
+    print("     -------------------")
+    print("C    I     I     I     I")
+    print("     -------------------")
+
+openGame()
